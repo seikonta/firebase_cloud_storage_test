f3a611885003b8b6b820a4a47a985111c6d864f5
Kon <s.takasu0602@gmail.com>
2021-12-29 20:47:48 +0900

    Android Firebase Cloud Storage Project for learning

--- app/src/main/java/app/takasu/kon/firebasecloudstoragetest/MainActivity.kt
+++ app/src/main/java/app/takasu/kon/firebasecloudstoragetest/MainActivity.kt
@@ -9,7 +9,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
-    var storage = Firebase.storage("gs://fir-cloud-storage-test-aafdf.appspot.com")
+    var storage = Firebase.storage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +19,23 @@ class MainActivity : AppCompatActivity() {
         var imageRef = storageRef.child("images")
 
         val fileName = "utokyo.png"
-        var utokyoRef = imageRef.child(fileName)
-//        var utokyoRef = imageRef.child("images/utokyo.png")
+//        var utokyoRef = imageRef.child(fileName)
+        var utokyoRef = storageRef.child("images/utokyo.png")
 
         val nameTextView = findViewById<TextView>(R.id.nameTextView)
-        nameTextView.text = utokyoRef.name
+        nameTextView.text = utokyoRef.path
 
         val iconImageView = findViewById<ImageView>(R.id.iconImage)
 
-        Glide.with(this)
-            .load(utokyoRef)
-            .into(iconImageView)
+//        Glide.with(this)
+//            .load(utokyoRef)
+//            .placeholder(R.drawable.seiko_icon)
+//            .into(iconImageView)
+
+        utokyoRef.downloadUrl.addOnSuccessListener { url ->
+            Glide.with(this)
+                .load(url)
+                .into(iconImageView)
+        }
     }
 }
